1bc3bbc3b1f11e3b72eac3f50f500b414b42e7f7
tobi030607 <tobiasschiechel@gmail.com>
2026-03-12 14:56:41 +0100

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,27 @@
+public class Card {
+
+    private Suit suit;
+    private Rank rank;
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public int getValue() {
+        return rank.getValue();
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
